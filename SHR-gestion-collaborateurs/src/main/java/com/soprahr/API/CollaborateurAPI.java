5ad1b9c86878526c833a925d78a91a17ea6f29e1
BEN MILED Khalil <khalil.benmiled@soprahr.com>
2020-02-20 11:11:34 +0100

    RabbitMQ configuration

--- SHR-gestion-collaborateurs/src/main/java/com/soprahr/API/CollaborateurAPI.java
+++ SHR-gestion-collaborateurs/src/main/java/com/soprahr/API/CollaborateurAPI.java
@@ -2,11 +2,14 @@ package com.soprahr.API;
 
 import java.util.List;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soprahr.RabbitMQ.RabbitMQSender;
 import com.soprahr.Repository.CollaborateurRepository;
 import com.soprahr.models.Collaborateur;
 
@@ -17,10 +20,22 @@ public class CollaborateurAPI {
 	
 	@Autowired
 	public CollaborateurRepository repository;
+	@Autowired
+	RabbitMQSender rabbitMQSender;
+	
+	@RabbitListener(queues = "EventFromSalle")
+	public void recievedMessage(String msg) {
+		System.out.println("Recieved Message From RabbitMQ: " + msg);
+	}
 	
 	@GetMapping
 	public List<Collaborateur> getAll(){
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public void Test() {
+		rabbitMQSender.send();
 	}
 
 }
--- SHR-gestion-salles/src/main/java/com/soprahr/API/SalleAPI.java
+++ SHR-gestion-salles/src/main/java/com/soprahr/API/SalleAPI.java
@@ -2,6 +2,8 @@ package com.soprahr.API;
 
 
 import javax.websocket.server.PathParam;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.soprahr.RabbitMQ.RabbitMQSender;
 import com.soprahr.Services.SalleServices;
 import com.soprahr.models.Salle;
 import net.minidev.json.JSONObject;
@@ -21,6 +25,13 @@ public class SalleAPI {
 
 	@Autowired
 	public SalleServices service;
+	@Autowired
+	public RabbitMQSender rabbitMQSender;
+	
+	@RabbitListener(queues = "MyQueue")
+	public void recievedMessage(String msg) {
+		System.out.println("Recieved Message From RabbitMQ: " + msg);
+	}
 
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public JSONObject addSalle(@RequestBody Salle salle) {
@@ -50,5 +61,10 @@ public class SalleAPI {
 	@GetMapping(value = "/verifDispoSalle" , produces = MediaType.APPLICATION_JSON_VALUE)
 	public JSONObject verifDispoSalle(@Param(value = "idSalle") int idSalle,@Param (value = "date1") String date) {
 		return service.verifDispoSalle(idSalle,date);
+	}
+	
+	@PostMapping(value = "/send")
+	public void Test() {
+		rabbitMQSender.send();
 	}
 }
